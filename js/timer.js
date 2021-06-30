e386c52ec6db0129a0dd19f4269d6e46b028b6e6
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-06-30 21:13:10 +0200

    1

--- js/timer.js
+++ js/timer.js
@@ -1,74 +1,52 @@
-"use strict";
-
 class CountdownTimer {
-  constructor(selector, targetDate) {
-    this.selector = selector;
-    this.targetDate = targetDate.getTime();
+  constructor({ selector, targetDate }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.selector = document.querySelector(`${selector}`);
+    this.onTick = this.updateTimerFields;
+    this.targetDate = targetDate;
+    this.init();
+  }
+  init() {
+    const time = this.getTimeComponents(this.targetDate.getTime() - Date.now());
+    this.onTick(time);
   }
 
   start() {
-    this.selectRefs();
-    const timerId = setInterval(() => {
-      const currentDate = Date.now();
-      const time = this.targetDate - currentDate;
-      if (time < 0) {
-        clearInterval(timerId);
-        return console.log("Your date is ago!");
-      }
-      this.updateTimerFace(time);
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate.getTime() - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.onTick(time);
     }, 1000);
   }
-
-  selectRefs() {
-    this.timerFace = document.querySelector(this.selector);
-    this.days = this.timerFace.querySelector(".value[data-value=days]");
-    this.hours = this.timerFace.querySelector(".value[data-value=hours]");
-    this.mins = this.timerFace.querySelector(".value[data-value=mins]");
-    this.secs = this.timerFace.querySelector(".value[data-value=secs]");
+  updateTimerFields({ days, hours, mins, secs }) {
+    this.selector.querySelector('[data-value="days"]').textContent = days;
+    this.selector.querySelector('[data-value="hours"]').textContent = hours;
+    this.selector.querySelector('[data-value="mins"]').textContent = mins;
+    this.selector.querySelector('[data-value="secs"]').textContent = secs;
   }
-
-  updateTimerFace(time) {
-    const daysValue = this.calculateForDays(time);
-    const hoursValue = this.calculateForHours(time);
-    const minsValue = this.calculateForMins(time);
-    const secsValue = this.calculateForSecs(time);
-
-    this.updateTextContent(this.days, daysValue);
-    this.updateTextContent(this.hours, hoursValue);
-    this.updateTextContent(this.mins, minsValue);
-    this.updateTextContent(this.secs, secsValue);
-  }
-
-  updateTextContent(elem, value) {
-    elem.textContent = `${value}`;
-  }
-
-  calculateForDays(time) {
-    return this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  }
-
-  calculateForHours(time) {
-    return this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
     );
-  }
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  calculateForMins(time) {
-    return this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    return { days, hours, mins, secs };
   }
-
-  calculateForSecs(time) {
-    return this.pad(Math.floor((time % (1000 * 60)) / 1000));
-  }
-
   pad(value) {
-    return String(value).padStart(2, 0);
+    return String(value).padStart(2, '0');
   }
 }
-
-const currentTime = Date.now();
-const clockTime = currentTime + 345600000;
-
-const newTimer = new CountdownTimer("#timer-1", new Date(clockTime));
-
-newTimer.start();
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Mar 01, 2021'),
+});
+timer.start.call(timer);
